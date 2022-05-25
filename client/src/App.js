b3b93231bea504195a4f71b4c7b08b84f1cb8b98
bnurmos <yuliaslinc@gmail.com>
2022-05-25 09:55:27 -0600

    Added raw NavBar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
+//import logo from './logo.svg';
+import NavBar from './components/Navbar/NavBar';
+//import './App.css';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      Hello Y, A, A
-      </header>
-         </div>
+    <div>
+      <NavBar />
+    </div>
   );
 }
 
