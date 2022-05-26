4b3cf4609da6901e4bba27e1a7f5bcadcd772ae3
Adrianchin <98614997+Adrianchin@users.noreply.github.com>
2022-05-25 19:41:08 -0600

    Updated with calculations

--- server/routes/facelandmark.js
+++ server/routes/facelandmark.js
@@ -38,45 +38,54 @@ async function facelandmark(req, res, next) {
     }
     await setEndpoint();
     
-    function euclideandistance(x1,x2,y1,y2,z1,z2){
-        return Math.sqrt((x2-x1)^2+(y2-y1)^2+(z2-z1)^2)
+    function euclideandistance(x1,y1,z1,x2,y2,z2){
+        return Math.sqrt((x2-x1)**2+(y2-y1)**2+(z2-z1)**2)
     }
-        let landmarks
-    
-        //used for eye ratio
-        let left_eye_top
-        let left_eye_bottom
-        let left_eye_right
-        let left_eye_left
-        //used for eye ratio
-        let right_eye_top 
-        let right_eye_bottom
-        let right_eye_right
-        let right_eye_left
-        //used for general face shape
-        let midpoint_between_eyes
-        let chin_gnathion
-        let left_ear_tragion
-        let right_ear_tragion
-        //used for cheek location as a function of chin
-        let left_cheek_center
-        let right_cheek_center
-        let left_chin_gonion
-        let right_chin_gonion
-        //used for general nose shape
-        let nose_tip
-        let nose_bottom_right
-        let nose_bottom_left
-        let nose_bottom_center
-    
-        let upper_lip
-        let lower_lip
-        let mouth_left
-        let mouth_right
-        let mouth_center
-    
-        let forehead_gabella
- 
+
+    let landmarks=imageInformation[0].faceAnnotations[0].landmarks
+    //used for eye ratio
+    let left_eye_top = landmarks[16].position
+    let left_eye_bottom = landmarks[18].position
+    let left_eye_right_corner = landmarks[17].position
+    let left_eye_left_corner = landmarks[19].position
+    //used for eye ratio
+    let right_eye_top  = landmarks[20].position
+    let right_eye_bottom = landmarks[22].position
+    let right_eye_right_corner = landmarks[21].position
+    let right_eye_left_corner = landmarks[23].position
+    //used for general face shape
+    let midpoint_between_eyes = landmarks[6].position
+    let chin_gnathion = landmarks[29].position
+    let left_ear_tragion = landmarks[26].position
+    let right_ear_tragion = landmarks[27].position
+    //used for cheek location as a function of chin
+    let left_cheek_center = landmarks[32].position
+    let right_cheek_center = landmarks[33].position
+    let left_chin_gonion = landmarks[30].position
+    let right_chin_gonion = landmarks[31].position
+    //used for general nose shape
+    let nose_tip = landmarks[7].position
+    let nose_bottom_right = landmarks[13].position
+    let nose_bottom_left = landmarks[14].position
+    let nose_bottom_center = landmarks[15].position
+
+    let upper_lip = landmarks[8].position
+    let lower_lip = landmarks[9].position
+    let mouth_left = landmarks[10].position
+    let mouth_right = landmarks[11].position
+    let mouth_center = landmarks[12].position
+
+    let forehead_gabella = landmarks[28].position
+    console.log(left_eye_top.x, left_eye_top.y, left_eye_top.z)
+    console.log(left_eye_top)
+    console.log(left_eye_bottom)
+    console.log(left_eye_right_corner)
+    console.log(left_eye_left_corner)
+    leftEyeVertD=euclideandistance(left_eye_top.x, left_eye_top.y, left_eye_top.z,left_eye_bottom.x, left_eye_bottom.y, left_eye_bottom.z)
+    leftEyeHorrizD=euclideandistance(left_eye_right_corner.x, left_eye_right_corner.y, left_eye_right_corner.z,left_eye_left_corner.x, left_eye_left_corner.y, left_eye_left_corner.z)
+    console.log("Euclidiean distance for vertical left eye dimension: ", leftEyeVertD)
+    console.log("Euclidiean distance for horizontal left eye dimension: ", leftEyeHorrizD)
+    console.log("The ratio of vertical/horizontal left eye dimensions is: ", leftEyeVertD/leftEyeHorrizD)
     return {imageInformation, originalImageSize}
 }
 
