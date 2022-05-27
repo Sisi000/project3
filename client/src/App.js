f375444e1a0deab6a4bc8ca039e8b04e682b4043
bnurmos <bnurmos@gmail.com>
2022-05-26 23:20:54 -0600

    Added routes, home+about pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,25 @@
 //import logo from './logo.svg';
 import NavBar from './components/Navbar/NavBar';
+import About from './components/About/about';
+import Home from './components/Home/home';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 //import './App.css';
 
 function App() {
   return (
-    <div>
+    <>
+    <Router>
+      <div className='App'>
       <NavBar />
-    </div>
+      <div className='content'>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+      </div>
+    </Router>
+    </>
   );
 }
 
---
+++ client/src/components/Home/home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Container} from "react-bootstrap";
+
+
+function Home() {
+  return (
+    <div>
+      <Container fluid>
+        <h1>Welcome to Home page!</h1>
+      </Container>
+    </div>
+  );
+}
+
+export default Home;
