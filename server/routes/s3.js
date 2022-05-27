cce8e173130729bee29f31658c6e9c69353be13d
Sanja <sivansic@gmail.com>
2022-05-27 16:21:44 -0600

    upload to vision works

--- server/routes/s3.js
+++ server/routes/s3.js
@@ -7,17 +7,19 @@ const aws = require("aws-sdk");
 const { getFileStream } = require("../db/models/s3Model");
 const s3 = new aws.S3();
 const mongoose = require("../db/mongoose");
+const { facelandmark } = require("../db/models/faceDetectionCalc.js");
 
-const storage = s3Storage({
-  s3,
-  Bucket: "project3inc",
-  resize: {
-    width: 1000,
-    height: 1000,
-    options: { withoutEnlargement: true },
-  },
-});
+// const storage = s3Storage({
+//   s3,
+//   Bucket: "project3inc",
+//   resize: {
+//     width: 1000,
+//     height: 1000,
+//     options: { withoutEnlargement: true },
+//   },
+// });
 
+const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
 const { Schema, model } = mongoose;
@@ -35,21 +37,20 @@ router.get("/images/:key", (req, res) => {
   readStream.pipe(res);
 });
 
-// router.get("/allimages", async (req, res) => {
-//   try {
-//     debug("getting all images");
-//     const allImages = getAllImages();
-//     res.send(allImages);
-//   } catch (err) {
-//     debug(err.message);
-//   }
-// });
 
 router.post("/upload", upload.single("image"), async (req, res, next) => {
-  console.log(req.file); // Print upload details
-  const result = await Photo.create({ location: req.file.Location });
-  console.log("result is", result);
-  res.send("Successfully uploaded!");
+  console.log("Uploaded file is", req.file.buffer); // Print upload details
+  // const result = await Photo.create({ location: req.file.Location });
+  // console.log("result is", result);
+  // res.send("Successfully uploaded!");
+  const buffer = req.file.buffer;
+  const resultVision = await facelandmark(buffer);
+  console.log("resultVision is", resultVision);
+  // const result = async () => {
+  //   // const file = req.file;
+  //   const result2 = upload2.single("image");
+  //   console.log("result is", result2);
+    res.send(resultVision);
 });
 
 module.exports = router;
