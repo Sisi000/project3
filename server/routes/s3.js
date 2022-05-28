e116572f4b74c5af91b7b5dd0bd59db6ed418a51
Sanja <sivansic@gmail.com>
2022-05-27 18:16:30 -0600

    changes to upload

--- server/routes/s3.js
+++ server/routes/s3.js
@@ -5,11 +5,17 @@ const multer = require("multer");
 const s3Storage = require("multer-sharp-s3");
 const aws = require("aws-sdk");
 const { uploadFile, getFileStream } = require("../db/models/s3Model");
-const s3 = new aws.S3();
 const mongoose = require("../db/mongoose");
 const { facelandmark } = require("../db/models/faceDetectionCalc.js");
+const sharp = require('sharp');
 
-// const storage = s3Storage({
+const s3 = new aws.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+});
+
+
+// const storage2 = s3Storage({
 //   s3,
 //   Bucket: "project3inc",
 //   resize: {
@@ -19,8 +25,8 @@ const { facelandmark } = require("../db/models/faceDetectionCalc.js");
 //   },
 // });
 
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
+// const storage = multer.memoryStorage();
+// const upload = multer({ storage: storage });
 // const upload2 = multer({ storage: storage2 });
 
 const { Schema, model } = mongoose;
@@ -38,19 +44,48 @@ router.get("/images/:key", (req, res) => {
   readStream.pipe(res);
 });
 
-router.post("/upload", upload.single("image"), async (req, res, next) => {
+// router.post("/upload", upload.single("image"), async (req, res, next) => {
+router.post("/upload", multer().single("image"), (req, res, next) => {
   console.log("Uploaded file is", req.file.buffer); // Print upload details
-  // const result = await Photo.create({ location: req.file.Location });
-  // console.log("result is", result);
-  // res.send("Successfully uploaded!");
-  const buffer = req.file.buffer;
-  const resultVision = await facelandmark(buffer);
-  console.log("resultVision is", resultVision);
+  // const buffer = req.file.buffer;
+  Promise.all([
+    s3.upload({
+      Bucket: "project3inc",
+      Key: req.file.key,
+      Body: req.file.buffer
+    }),
+
+       
+    sharp(req.file.buffer)
+      .resize(400, 300)
+      .toBuffer()
+      .then(buffer => {
+        const resultVision = facelandmark(buffer)
+        console.log(resultVision);
+        // res.send(resultVision);
+        // .promise()
+    //  .then(toMongo => {
+
+    //    const result = await Photo.create({ location: req.file.Location });
+    //     }
+      }
+        ),
+        
+        
+        // console.log("resultVision is", result);
+
+  // const resultVision = await facelandmark(buffer);
   // const result = async () => {
-  //   // const file = req.file;
-  //   const result2 = upload2.single("image");
-  //   console.log("result is", result2);
-    res.send(resultVision);
+    //   // const file = req.file;
+    //   const result2 = upload2.single("image");
+    //   console.log("result is", result2);
+    // res.send("Successfully uploaded!");
+  ])
+  .then(() => res.send("Image uploaded!"))
+  .catch(e => {
+    console.warn(e) // debug this error
+    res.status(500).send("Unable to upload images")
+  })
 });
 
 module.exports = router;
