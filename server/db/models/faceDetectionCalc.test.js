8142c5a76695b1b702c5cc054ba3292721e32b86
Adrianchin <98614997+Adrianchin@users.noreply.github.com>
2022-05-30 10:36:17 -0600

    Updated with working Jest env (see setup file)

--- server/db/models/faceDetectionCalc.test.js
+++ server/db/models/faceDetectionCalc.test.js
@@ -1,7 +1,12 @@
 const {facelandmark} = require("./faceDetectionCalc");
 const fs = require ('fs');
 
+
 test("Tests for image submit to Google API", async () => {
+
+    //console.log("api keys: ", process.env.GOOGLE_API_EMAIL)
+    //console.log("api keys: ", process.env.GOOGLE_API_KEY)
+
     let imageFileUpload = fs.readFileSync("./testimages/testimage.jpg"); 
     let facelandmarkResult = await facelandmark(imageFileUpload);
     expect(facelandmarkResult).toBeDefined()
