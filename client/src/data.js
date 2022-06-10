75b0569138f6ee7cf45f68e75644e5e3bd58ea57
bnurmos <bnurmos@gmail.com>
2022-06-09 23:47:32 -0600

    added slider.js to a home page

---
+++ client/src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://www.eyeconic.com/on/demandware.static/-/Sites/default/dw92e6b825/images/eyeconic/plp/eyewear/2022/eyeglasses-womens-eyewear-mobile-spring@2x.jpg",
+      title: "SPRING SALE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 50% OFF FOR NEW ARRIVALS.",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "https://ae01.alicdn.com/kf/H6312fe97633c44028dcf4d970b249ad5K/Masculine-Luxury-Sunglasses-Rimless-Rectangular-2021-Trendy-Square-Men-Women-Shades-Dark-Brown-Frameless-Sunglasses-Gafas.jpg",
+      title: "SUMMER COLLECTION",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 15% OFF FOR NEW ARRIVALS.",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "https://cdn.shopify.com/s/files/1/0601/5415/1102/files/glasses-2020-700x700.jpg",
+      title: "LOUNGEWEAR LOVE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 10% OFF FOR NEW ARRIVALS.",
+      bg: "fbf0f4",
+    },
+  ];
