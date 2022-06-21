1b505e2bbebefff8b798995c8f87cff4b47621b7
bnurmos <bnurmos@gmail.com>
2022-06-20 22:18:33 -0600

    Left just one card

--- client/src/components/Home/tryOn.js
+++ client/src/components/Home/tryOn.js
@@ -1,8 +1,8 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.css';
 import { Container, Card } from "react-bootstrap";
-import Images from "../../assets/tryon.jpg"
-import Images2 from "../../assets/tryon2.jpg"
+import Images from "../../assets/tryon2.jpg"
+
 function TryOn() {
   return (
     <Container fluid style={{ padding: "0" }} className="pb-5">
@@ -10,7 +10,7 @@ function TryOn() {
 
       <Card>
      
-        <Card.Img src={Images2} alt="Card image" />
+        <Card.Img src={Images} alt="Card image" />
         <Card.ImgOverlay>
           <Card.Body className='position-absolute bottom-0 end-10 mx-5 mb-5 pb-5 px-5'>
             <a className="btn btn-warning btn-lg mx-5 mb-5 px-5" href="/upload" role="button"><h1>VIRTUAL TRY-ON</h1></a>
@@ -18,15 +18,6 @@ function TryOn() {
         </Card.ImgOverlay>
       </Card>
 
-      <Card>
-        <Card.Img src={Images} alt="Card image" />
-        <Card.ImgOverlay >
-          <Card.Body className='position-absolute bottom-0 end-0 mx-5 mb-5 pb-5 px-5'>
-           
-            <a className="btn btn-lg btn-primary mx-5 mb-5 px-5" href="/upload" role="button"><h1>VIRTUAL TRY-ON</h1></a>
-          </Card.Body>
-        </Card.ImgOverlay>
-      </Card>
 
     </Container>
   );
