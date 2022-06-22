47bb54180bc8d2bdf9fc77c0056ffce7a3b5061a
bnurmos <bnurmos@gmail.com>
2022-06-22 11:14:45 -0600

    changed button position

--- client/src/components/Home/tryOn.js
+++ client/src/components/Home/tryOn.js
@@ -5,15 +5,13 @@ import Images from "../../assets/tryon2.jpg"
 
 function TryOn() {
   return (
-    <Container fluid style={{ padding: "0" }} className="pb-5">
+    <Container fluid style={{ padding: "0" }} className="py-5 my-5">
       
-
       <Card>
-     
         <Card.Img src={Images} alt="Card image" />
         <Card.ImgOverlay>
           <Card.Body className='position-absolute bottom-0 end-10 mx-5 mb-5 pb-5 px-5'>
-            <a className="btn btn-warning btn-lg mx-5 mb-5 px-5" href="/upload" role="button"><h1>VIRTUAL TRY-ON</h1></a>
+            <a className="btn btn-warning btn-lg mx-5 px-5" href="/upload" role="button"><h1>VIRTUAL ASSISTANT</h1></a>
           </Card.Body>
         </Card.ImgOverlay>
       </Card>
