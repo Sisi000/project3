669f743a84ad2db37b52512aa7afeb3e674bea59
bnurmos <bnurmos@gmail.com>
2022-07-04 20:56:15 -0600

    Switched numers type to string

--- server/db/models/prescriptionModel.js
+++ server/db/models/prescriptionModel.js
@@ -5,13 +5,13 @@ const PrescriptionSchema = new mongoose.Schema(
     SphereR: { type: Number, required: true },
     CylinderR: { type: Number, required: true },
     AxisR: { type: Number, required: true },
-    ADDR: { type: Number, required: true },
+    ADDR: { type: String, required: true },
     SphereL: { type: Number, required: true },
     CylinderL: { type: Number, required: true },
     AxisL: { type: Number, required: true },
-    ADDL: { type: Number, required: true },
-    RPD: { type: Number, required: true },
-    LPD: { type: Number, required: true },
+    ADDL: { type: String, required: true },
+    RPD: { type: String, required: true },
+    LPD: { type: String, required: true },
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
   },
   {
