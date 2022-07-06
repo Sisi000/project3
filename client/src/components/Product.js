558805fcf79a6831a96f22ffb6717e1e02b21b74
Sanja <sivansic@gmail.com>
2022-07-06 11:25:53 -0600

    Added onMouseOver and Out to product

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -31,7 +31,7 @@ function Product(props) {
   return (
     <Card>
       <Link to={`/product/${product.slug}`}>
-        <img src={product.image} className="card-img-top" alt={product.slug} />
+        <img src={product.image} className="card-img-top" alt={product.slug} onMouseOver={e => e.currentTarget.src = product.images} onMouseOut={e => (e.currentTarget.src = product.image)}/>
       </Link>
       <Card.Body>
         <Link  style={{textDecoration: 'none'}} to={`/product/${product.slug}`}>
