0bf91d4c4c052b8b4fcdb14ed450dc2b4447ed86
Adrianchin <98614997+Adrianchin@users.noreply.github.com>
2022-07-18 11:05:32 -0600

    Update for cors

--- client/src/components/AxiosHelper.js
+++ client/src/components/AxiosHelper.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 const server = process.env.REACT_APP_SERVER;
+const client = process.env.REACT_APP_CLIENT;
 
 //Used
 export async function axiosPost(endpoint, payload){
@@ -15,6 +16,7 @@ export async function axiosPostMPFD(endpoint,formData) {
       url: server+endpoint,
       data: formData,
       headers: {
+        "Access-Control-Allow-Origin": client,
         "Content-Type": "multipart/form-data",
       },
     });
@@ -27,6 +29,7 @@ export async function axiosPostMPFD(endpoint,formData) {
       url: server+endpoint,
       data: formData,
       headers: {
+        "Access-Control-Allow-Origin": client,
         "Content-Type": "multipart/form-data",
           authorization: `Bearer ${userInfo}`
       },
@@ -38,7 +41,11 @@ export async function axiosPostAuth(endpoint, payload ,userInfo){
     return await axios.post(
         server+endpoint,
         payload,
-        {headers: {authorization: `Bearer ${userInfo}`},
+        {
+          headers: {
+            "Access-Control-Allow-Origin": client,
+            authorization: `Bearer ${userInfo}`
+          },
         }
       );
 }
@@ -48,42 +55,72 @@ export async function axiosPostFinalEndpointAuth(endpoint,product_id,finalEndpoi
         server+endpoint+product_id+finalEndpoint,
         payload,
         {
-          headers: { Authorization: `Bearer ${userInfo}` },
+          headers: { 
+            "Access-Control-Allow-Origin": client,
+            Authorization: `Bearer ${userInfo}` 
+          },
         }
       );
 }
 //Used
 export async function axiosDeletePost(endpoint,filename ){
-  return await axios.post(server+endpoint, { Key: filename });
+  return await axios.post(server+endpoint, 
+    { 
+      Key: filename ,
+      headers: {
+        "Access-Control-Allow-Origin": client,
+      }
+    }
+    );
 }
 
 //Used
 export async function axiosGet(endpoint){
-    return await axios.get(server+endpoint);
+    return await axios.get(server+endpoint,
+      {
+        headers: {
+          "Access-Control-Allow-Origin": client,
+        }
+      });
   }
   
   //Used
   export async function axiosGetID(endpoint, id){
-    return await axios.get(server+endpoint+id);
+    return await axios.get(server+endpoint+id,
+      {
+        headers: {
+          "Access-Control-Allow-Origin": client,
+        }
+      }
+      );
 }
 
 //Used
 export async function axiosGetAuth(endpoint, userInfo){
     return await axios.get(server+endpoint, {
-        headers: { Authorization: `Bearer ${userInfo}` },
+        headers: { 
+         "Access-Control-Allow-Origin": client,
+          Authorization: `Bearer ${userInfo}` 
+      },
       });
     }
     //Used
     export async function getParamsAuth(endpoint, detail, userInfo){
       return await axios.get(server+endpoint+detail, {
-        headers: { Authorization: `Bearer ${userInfo}` },
+        headers: { 
+          "Access-Control-Allow-Origin": client,
+          Authorization: `Bearer ${userInfo}` 
+        },
       });
     }
     
     //Used
     export async function axiosDeleteByIDAuth(endpoint, id, userInfo){
       return await axios.delete(server+endpoint+id, {
-        headers: { Authorization: `Bearer ${userInfo}` },
+        headers: { 
+          "Access-Control-Allow-Origin": client,
+          Authorization: `Bearer ${userInfo}` 
+        },
       });
     }
     
@@ -93,7 +130,10 @@ export async function axiosGetAuth(endpoint, userInfo){
         server+endpoint,
         payload,
         {
-          headers: { Authorization: `Bearer ${userInfo}` },
+          headers: { 
+            "Access-Control-Allow-Origin": client,
+            Authorization: `Bearer ${userInfo}` 
+          },
         }
         );
       };
@@ -104,7 +144,10 @@ export async function axiosGetAuth(endpoint, userInfo){
           server+endpoint+order+finalEndpoint,
           payload,
           {
-            headers: { authorization: `Bearer ${userInfo}` },
+            headers: { 
+              "Access-Control-Allow-Origin": client,
+              authorization: `Bearer ${userInfo}` 
+            },
           }
           );
         }
@@ -114,7 +157,10 @@ export async function axiosGetAuth(endpoint, userInfo){
         server+endpoint+Id,
         payload,
         {
-          headers: { Authorization: `Bearer ${userInfo}` },
+          headers: { 
+            "Access-Control-Allow-Origin": client,
+            Authorization: `Bearer ${userInfo}` 
+          },
         }
         );
       };
