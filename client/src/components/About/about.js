a246185f4c590b7ededa0c43b5e239df6c05f632
Adrianchin <98614997+Adrianchin@users.noreply.github.com>
2022-07-18 20:27:59 -0600

    Updated about us

--- client/src/components/About/about.js
+++ client/src/components/About/about.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Container, Row, Col, Button, Card } from "react-bootstrap";
 import "font-awesome/css/font-awesome.min.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faGithub , faLinkedin, faGalacticRepublic} from "@fortawesome/free-brands-svg-icons";
 import "bootstrap/dist/css/bootstrap.css";
 import Images from "../images/developers/Adrian_Chin.jpg"
 import Image1 from "../images/developers/Sanja_Ivansic.jpg"
@@ -14,7 +14,7 @@ function About() {
     <div className="aboutStyles">
       <Container fluid>
         <Row>
-          <Col className="py-5 my-5 text-center">
+          <Col className="py-4 my-4 text-center">
             <h2>Mandate</h2>
             <p>
               Using AI and ML to revolutionize the Ecommerce experience.
@@ -24,7 +24,7 @@ function About() {
               Provide an interactive Ecommerce experience with a virtual optician for eyeglass recommendations from the comfort of your house.
             </p>
           </Col>
-          <Col className="py-5 my-5 text-center">
+          <Col className="py-4 my-4 text-center">
             <h2>Values</h2>
             <p>
               Take Ownership - Be responsabile for your work and take action.
@@ -34,6 +34,21 @@ function About() {
               <br/>
               <br/>
               Deliver Results - We get the job done. Actions speak louder than words. 
+            </p>
+          </Col>
+        </Row>
+        <Row>
+          <Col className="text-left">
+            <h2>About this application</h2>
+            <p>
+              This full stack application was created to help customers purchase eyeglass with the help of AI and ML.
+              <br/>
+              <br/>
+              MongoDB, Express.js, React and Node.js are used to create this full stack application. A PayPal Ecommerce payment system is implemented and the application is secured with BCrypt. This application is deployed and hosted in Heroku.
+              <br/>
+              <br/>
+              TensorFlow2 and Google Vision AI Convolutional Deep Neural Network models are used to analyze data submitted by the user. N-Dimensional Euclidean distances are calculated to classify products that suit your unique facial features. Calculations are unit tested and functional tested with coverage reports being generated.
+              <br/>
             </p>
           </Col>
         </Row>
@@ -56,6 +71,10 @@ function About() {
                     <br/>
                     Algorithms
                   </Card.Text>
+                  <a href="https://adrian-chin.herokuapp.com/" target="_blank" rel="noreferrer">
+                  <Button type='button' className='shadow-none' variant="primary"><FontAwesomeIcon className='px-2 mx-2 text-center' icon={faGalacticRepublic} size="2x"  /></Button></a>
+                  <a href="https://www.linkedin.com/in/adrianjchin/" target="_blank" rel="noreferrer">
+                  <Button type='button' className='shadow-none' variant="primary"><FontAwesomeIcon className='px-2 mx-2 text-center' icon={faLinkedin} size="2x"  /></Button></a>
                   <a href="https://github.com/Adrianchin" target="_blank" rel="noreferrer">
                   <Button type='button' className='shadow-none' variant="primary"><FontAwesomeIcon className='px-2 mx-2 text-center' icon={faGithub} size="2x"  /></Button></a>
                 </Card.Body>
