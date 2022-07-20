b8d54c5ceae76029163277edd7b2e380e7d306dc
bnurmos <bnurmos@gmail.com>
2022-07-20 13:01:42 -0600

    Revert "Fixed capital letters"
    This reverts commit 114e4f33db6bdb3340dd71991c80f3eacc4848d7.

--- client/src/components/Home/Slider.js
+++ client/src/components/Home/Slider.js
@@ -1,8 +1,8 @@
 import { ArrowLeftOutlined, ArrowRightOutlined } from "@mui/icons-material";
 import { useState } from "react";
 import styled from "styled-components";
-import { sliderItems } from "./Data";
-import { mobile } from "./Responsive";
+import { sliderItems } from "./data";
+import { mobile } from "./responsive";
 
 const Container = styled.div`
   width: 98.7vw;
