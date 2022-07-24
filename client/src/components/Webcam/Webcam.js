ccc51836484bc6291f5730756f3702bf07c88a0a
Sanja <sivansic@gmail.com>
2022-07-23 18:18:02 -0600

    Added scan to webcam

--- client/src/components/Webcam/Webcam.js
+++ client/src/components/Webcam/Webcam.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import Webcam from "react-webcam";
-import {axiosPostMPFD, axiosPost} from "../AxiosHelper";
+import { axiosPostMPFD, axiosPost } from "../AxiosHelper";
 import { Container, Row, Col } from "react-bootstrap";
 import "./Webcam.css";
 import Product from "../Product";
-import { ConstructionOutlined } from "@mui/icons-material";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import scan from "../../assets/scan.gif";
 
 const videoConstraints = {
   width: 400,
@@ -23,10 +25,10 @@ function WebcamCapture(props) {
 
   async function postImage(filesForUpload) {
     const formData = new FormData();
-    formData.append("image", filesForUpload); 
-    formData.append("filters", filterJSON); 
+    formData.append("image", filesForUpload);
+    formData.append("filters", filterJSON);
     const endpoint = "/uploadwebcam";
-    const result = await axiosPostMPFD(endpoint,formData);
+    const result = await axiosPostMPFD(endpoint, formData);
     return result;
   }
 
@@ -38,13 +40,13 @@ function WebcamCapture(props) {
   const showSuggestedCam = async (result) => {
     const params = result;
     const endpoint = `/api/products/id`;
-    const result2 = await axiosPost(endpoint, {params});
+    const result2 = await axiosPost(endpoint, { params });
     setProducts(result2.data);
   };
 
   const submit = async (event) => {
     event.preventDefault();
-    if(file){
+    if (file) {
       const result = await postImage(file)
         .then(async (result) => {
           await showSuggestedCam(result);
@@ -53,22 +55,43 @@ function WebcamCapture(props) {
           console.log(err);
         });
       setFile(result.image);
-    }else{
-      console.log("No file taken, please take picture")
+    } else {
+      console.log("No file taken, please take picture");
     }
   };
+
+  const [show, setShow] = useState(false);
+
+  const handleShow = () => {
+    setShow(true);
+    setTimeout(() => setShow(false), 3800);
+  };
+
+  const [isHide, setIsHide] = useState(true);
+  if (products.length > 0) {
+    setTimeout(() => setIsHide(false), 2800);
+    clearTimeout(setIsHide);
+  }
 
   return (
     <div className="webcam-container">
       <div className="container-uploads">
         <form className="forms3" onSubmit={submit}>
-        <b>Instructions:</b>
-        <p>Optional: Tell the virtual optician a few preferences with the filters</p>
-        <p>1) Ensure the subject is facing the camera with their face fully visible.</p>
-        <p>2) Capture the picture on the webcam. Retake the picture if needed</p>
-        <p>3) Upload the picture to the virtual optician.</p>
-        <p>4) See what products fit the face!</p>
-        <br/>
+          <b>Instructions:</b>
+          <p>
+            Optional: Tell the virtual optician a few preferences with the
+            filters
+          </p>
+          <p>
+            1) Ensure the subject is facing the camera with their face fully
+            visible.
+          </p>
+          <p>
+            2) Capture the picture on the webcam. Retake the picture if needed
+          </p>
+          <p>3) Upload the picture to the virtual optician.</p>
+          <p>4) See what products fit the face!</p>
+          <br />
 
           <div className="webcam-img">
             {file === "" ? (
@@ -108,22 +131,40 @@ function WebcamCapture(props) {
               </button>
             )}
           </div>
-          <button className="btn btn-primary" type="submit">
+
+          <Button
+            className="btn btn-primary"
+            onClick={handleShow}
+            type="submit"
+          >
             Submit
-          </button>
+          </Button>
+          <Modal style={{ visibility: "hidden" }} show={show}>
+            <Modal.Body style={{visibility: "visible", marginTop: "130px"}}>
+              <img src={scan} style={{ width: "100%" }}></img>
+            </Modal.Body>
+          </Modal>
         </form>
       </div>
       <Container fluid style={{ padding: "0" }}>
         <Container className="mt-3">
           <div>
-            <div className="products my-5 py-2">
+            <div id="product-delayed" className="products my-5 py-2">
               <Row>
-              {products.length > 0 && <h1>Suggested Glasses</h1>}
-                {products.map((product) => (
-                  <Col key={product._id} sm={6} md={4} lg={4} className="mb-3">
-                    <Product product={product}></Product>
-                  </Col>
-                ))}
+                {!isHide && <h1>Suggested Glasses</h1>}
+                {!isHide
+                  ? products.map((product) => (
+                      <Col
+                        key={product._id}
+                        sm={6}
+                        md={4}
+                        lg={4}
+                        className="mb-3"
+                      >
+                        <Product product={product}></Product>
+                      </Col>
+                    ))
+                  : null}
               </Row>
             </div>
           </div>
